5daa3a6601d0f502946a86aafb36e7195fd49e63
agent1red <darklen14@gmail.com>
2019-04-21 13:02:44 -0700

    updated comic book detail view. Moved C# variables to the Detail Controller file

--- src/ComicBookStuff/ComicBookStuff/Controllers/ComicBooksController.cs
+++ src/ComicBookStuff/ComicBookStuff/Controllers/ComicBooksController.cs
@@ -12,6 +12,22 @@ namespace ComicBookStuff.Controllers
         public ActionResult Detail()
         {
 
+            ViewBag.SeriesTitle = "The Amazing Spider-man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' " +
+                        "life and his one, last, great act of revenge! Even if Spider-Man " +
+                        "survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
+
             return View();
 
 
