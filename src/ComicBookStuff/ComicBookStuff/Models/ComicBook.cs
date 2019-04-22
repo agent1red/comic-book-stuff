7aa303a8f695fe3e8d5583fd4de0c4c4269df083
agent1red <darklen14@gmail.com>
2019-04-21 18:41:31 -0700

    added comic book and artist models

---
+++ src/ComicBookStuff/ComicBookStuff/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookStuff.Models
+{
+    public class ComicBook
+    {
+        //attributes 
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
