03d40af8a3700e6b286f08ad9fcce03e87abf542
agent1red <darklen14@gmail.com>
2019-04-21 19:42:16 -0700

    updated comic book detail view to be strongly typed (classes)

--- src/ComicBookStuff/ComicBookStuff/Controllers/ComicBooksController.cs
+++ src/ComicBookStuff/ComicBookStuff/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookStuff.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,23 +13,25 @@ namespace ComicBookStuff.Controllers
         public ActionResult Detail()
         {
 
-            ViewBag.SeriesTitle = "The Amazing Spider-man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' " +
-                        "life and his one, last, great act of revenge! Even if Spider-Man " +
-                        "survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicbook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' " +
+                        "life and his one, last, great act of revenge! Even if Spider-Man " +
+                        "survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slott", Role = "Script"},
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"}
+                }
             };
 
 
-
-            return View();
+            return View(comicbook);
 
 
         }
